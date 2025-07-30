b421e8fe1d6d31c8daa6a2694a534592b1aa9f2b
Joselito Guardado <jaguardado123@gmail.com>
2025-07-30 16:45:45 -0500

    fixed bugs

--- lib/screens/screen1.dart
+++ lib/screens/screen1.dart
@@ -9,7 +9,6 @@ class Screen1 extends StatefulWidget {
 
 class _Screen1State extends State<Screen1> {
   // CREATE VARIABLE TO KEEP TRACK OF COUNTER INCREMENT
-  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -37,24 +36,10 @@ class _Screen1State extends State<Screen1> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               // CREATE CODE TO DISPLAY CURRENT COUNTER VALUE
-              const Text("Counter:"),
-              Text(
-                "${_counter}",
-                style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-              )
             ],
           ),
         ),
         // CREATE FLOATING ACTION BUTTON TO INCREMENT COUNTER
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _counter++;
-            });
-          },
-          child: const Icon(Icons.add),
-        ),
       ),
     );
   }
